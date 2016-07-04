05a1dd6a5003502e79a5e2bbb5bacb97e7f2b637
Beau Cronin <beau.cronin@gmail.com>
2016-07-03 21:58:04 -0700

    fix set_user_vars

--- docker_files/set_user_vars.py
+++ docker_files/set_user_vars.py
@@ -5,7 +5,7 @@ vars = toml.load('/cesm_vars.toml')
 
 def process(filename, vars):
     for k, v in vars.items():
-        subprocess.run("./xmlchange -file {} -id {} -val {}".format(filename, k, v))
+        subprocess.run("./xmlchange -file {} -id {} -val \"{}\"".format(filename, k, v), shell=True)
 
 for k, v in vars.items():
     if k == 'main':
