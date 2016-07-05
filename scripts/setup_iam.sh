ec71254e39221d050727a7a5e82ecf708a25190b
Beau Cronin <beau.cronin@gmail.com>
2016-07-04 19:08:21 -0700

    update iam roles to include dynamo

--- scripts/setup_iam.sh
+++ scripts/setup_iam.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-aws iam create-role --role-name s3access --assume-role-policy-document file://ec2-role-trust-policy.json
-aws iam put-role-policy --role-name s3access --policy-name S3-Permissions --policy-document file://ec2-role-access-policy.json
-aws iam create-instance-profile --instance-profile-name S3-Permissions
-aws iam add-role-to-instance-profile --instance-profile-name S3-Permissions --role-name s3access
+aws iam create-role --role-name sunshine-executor --assume-role-policy-document file://ec2-role-trust-policy.json
+
+aws iam put-role-policy --role-name sunshine-executor --policy-name S3-Permissions --policy-document file://ec2-role-s3-access-policy.json
+aws iam put-role-policy --role-name sunshine-executor --policy-name Dynamo-Permissions --policy-document file://ec2-role-dynamo-access-policy.json
+
+aws iam create-instance-profile --instance-profile-name Sunshine-Executor
+aws iam add-role-to-instance-profile --instance-profile-name Sunshine-Executor --role-name sunshine-executor
